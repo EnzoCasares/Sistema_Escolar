9830eeaf30bcfe024577b8a510b83cd522c16d00
RodrigoBolgheroni <rodrigobolgheroni1@gmail.com>
2026-03-08 18:06:33 -0300

    feat: Adiciona sistema de alunos(Observacoes,Notas,Disciplinas)

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import cadastro
+from app.api.v1.endpoints import cadastro, aluno
 
 api_router = APIRouter()
 api_router.include_router(cadastro.router, prefix="/cadastro", tags=["Cadastro"])
+api_router.include_router(aluno.router, prefix="/aluno", tags=["Aluno"])
